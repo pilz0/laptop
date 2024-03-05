8e11086b9e7f71b021aade64958d1553a71b9c24
pilz0 <marie0@riseup.net>
2024-03-05 21:00:03 +0100

    added some allow unfree stuff

--- configuration.nix
+++ configuration.nix
@@ -261,6 +261,10 @@
     programs.git.config.user.email = "marie0@riseup.net";
 
 
+  environment.sessionVariables = {
+    NIXPKGS_ALLOW_UNFREE="1";
+    };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
